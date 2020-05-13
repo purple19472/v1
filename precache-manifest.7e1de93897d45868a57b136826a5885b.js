f8cab933f6714a702fbb9473ea28dcdd16c814fa
Elaina Smith <smit8561@umn.edu>
2020-05-13 16:48:21 -0500

    Updates

---
+++ precache-manifest.7e1de93897d45868a57b136826a5885b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73daeca0c69d0fde7c958036bccbe453",
+    "url": "/v1/index.html"
+  },
+  {
+    "revision": "84d3984a21bbc26f4629",
+    "url": "/v1/static/css/main.e85c11ad.chunk.css"
+  },
+  {
+    "revision": "eb32e8512f5372779837",
+    "url": "/v1/static/js/2.69ec6f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/v1/static/js/2.69ec6f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84d3984a21bbc26f4629",
+    "url": "/v1/static/js/main.4f5fbe1b.chunk.js"
+  },
+  {
+    "revision": "903c1fa48a09020ad181",
+    "url": "/v1/static/js/runtime-main.10dfcfe3.js"
+  }
+]);
